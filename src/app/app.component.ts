0bc460dbbff0c61a8f64bb15a239eced4f04ae4b
JD-SWaresol <juan.delgado@smartwaresol.com>
2024-12-24 11:02:52 -0600

    01_Create a Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Ang_04_Tour_of_Heores';
+  title = 'Tour_of_Heores';
 }
