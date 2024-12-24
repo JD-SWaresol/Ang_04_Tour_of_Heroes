46d9c8a11bf66ee89edbe8ca100f3f71eacc63ad
JD-SWaresol <juan.delgado@smartwaresol.com>
2024-12-24 12:01:01 -0600

    02_The hero editor

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Hero } from '../../interface/hero';
+import { UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-heroes',
+  imports: [UpperCasePipe, FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+}
